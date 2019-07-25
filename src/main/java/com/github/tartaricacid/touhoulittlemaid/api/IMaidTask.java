4e23c390b2d3677bfbd25184647caedcf47262f3
Snownee <mjy523@126.com>
2019-07-26 01:44:10 +0800

    Revert "修复新commit的问题"
    This reverts commit 434a70d78a970a16542626669595cc8f33dfc516.

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/api/IMaidTask.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/api/IMaidTask.java
@@ -1,12 +1,15 @@
 package com.github.tartaricacid.touhoulittlemaid.api;
 
-import javax.annotation.Nullable;
-
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
 
 /**
  * 女仆任务模式接口
@@ -71,7 +74,8 @@ public interface IMaidTask {
      *
      * @return 本地化的模式名称
      */
-    default String getTranslationKey() {
-        return "task." + getUid().getNamespace() + "." + getUid().getPath();
+    @SideOnly(Side.CLIENT)
+    default String getTaskI18n() {
+        return I18n.format("task." + getUid().getNamespace() + "." + getUid().getPath());
     }
 }
