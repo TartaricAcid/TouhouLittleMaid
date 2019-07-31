fd301b74c8c0f39e98694e96fe0d08626d78e39d
Snownee <mjy523@126.com>
2019-07-31 14:27:39 +0800

    喝牛奶的音效

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/internal/task/VanillaFeedHandler.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/internal/task/VanillaFeedHandler.java
@@ -4,6 +4,8 @@ import com.github.tartaricacid.touhoulittlemaid.api.task.FeedHandler;
 import com.github.tartaricacid.touhoulittlemaid.api.task.Trend;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
@@ -79,7 +81,9 @@ public class VanillaFeedHandler implements FeedHandler {
 
     @Override
     public ItemStack feed(ItemStack stack, EntityPlayer owner) {
-        // TODO: 2019/7/27 使用牛奶桶时释放音效
+        if (stack.getItemUseAction() == EnumAction.DRINK) {
+            owner.playSound(SoundEvents.ENTITY_GENERIC_DRINK, 0.5F, owner.world.rand.nextFloat() * 0.1F + 0.9F);
+        }
         return stack.getItem().onItemUseFinish(stack, owner.world, owner);
     }
 }
