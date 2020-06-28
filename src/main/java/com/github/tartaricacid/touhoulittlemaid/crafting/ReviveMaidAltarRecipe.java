96a9a5fc81f83720ecba6d6d3df5b55eb0476e8d
TartaricAcid <baka943@qq.com>
2020-06-28 16:13:20 +0800

    修复复活女仆手办后部分属性残留问题

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/crafting/ReviveMaidAltarRecipe.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/crafting/ReviveMaidAltarRecipe.java
@@ -6,12 +6,17 @@ import com.github.tartaricacid.touhoulittlemaid.block.BlockGarageKit;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.init.MaidBlocks;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttribute;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.Collection;
 import java.util.List;
 
 public class ReviveMaidAltarRecipe extends AltarRecipe {
@@ -26,7 +31,21 @@ public class ReviveMaidAltarRecipe extends AltarRecipe {
         maid.setPosition(pos.getX() + 0.5, pos.getY() + 0.8, pos.getZ() + 0.5);
         maid.readEntityFromNBT(BlockGarageKit.getEntityData(garageKit));
         maid.setHealth(maid.getMaxHealth());
+        removeAttribute(maid, SharedMonsterAttributes.KNOCKBACK_RESISTANCE);
+        removeAttribute(maid, SharedMonsterAttributes.MOVEMENT_SPEED);
+        removeAttribute(maid, SharedMonsterAttributes.FLYING_SPEED);
+        removeAttribute(maid, SharedMonsterAttributes.ATTACK_DAMAGE);
+        removeAttribute(maid, SharedMonsterAttributes.ARMOR);
+        removeAttribute(maid, SharedMonsterAttributes.ARMOR_TOUGHNESS);
         maid.setModelId(BlockGarageKit.getModelId(garageKit));
         return maid;
+    }
+
+    private void removeAttribute(EntityMaid maid, IAttribute attribute) {
+        IAttributeInstance attr = maid.getEntityAttribute(attribute);
+        Collection<AttributeModifier> modifiers = attr.getModifiers();
+        for (AttributeModifier i : modifiers) {
+            attr.removeModifier(i);
+        }
     }
 }
