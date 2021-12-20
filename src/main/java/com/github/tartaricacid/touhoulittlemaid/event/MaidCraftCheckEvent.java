3180d30cb3c7360c09839aad180ab3b227d43abe
TartaricAcid <baka943@qq.com>
2021-12-20 20:30:59 +0800

    修正错误 - 修正复活女仆时的错误

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/event/MaidCraftCheckEvent.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/event/MaidCraftCheckEvent.java
@@ -5,6 +5,7 @@ import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.api.event.AltarCraftEvent;
 import com.github.tartaricacid.touhoulittlemaid.capability.MaidNumHandler;
 import com.github.tartaricacid.touhoulittlemaid.capability.MaidNumSerializer;
+import com.github.tartaricacid.touhoulittlemaid.crafting.SpawnMaidRecipe;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.common.Mod;
@@ -15,8 +16,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class MaidCraftCheckEvent {
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onAltarCraft(AltarCraftEvent event) {
-        ResourceLocation maid = new ResourceLocation(TouhouLittleMaid.MOD_ID, "entity.passive.maid");
-        if (event.getAltarRecipe().getEntityId().equals(maid)) {
+        if (event.getAltarRecipe() instanceof SpawnMaidRecipe) {
             MaidNumHandler num = event.getPlayer().getCapability(MaidNumSerializer.MAID_NUM_CAP, null);
             if (num != null && !num.canAdd()) {
                 if (!event.getWorld().isRemote) {
