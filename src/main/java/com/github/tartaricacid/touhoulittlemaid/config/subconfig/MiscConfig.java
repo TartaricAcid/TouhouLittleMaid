1701bc40ff45312aa5f3b1f4e8240764357bb099
tartaric_acid <baka943@qq.com>
2023-09-27 17:37:24 +0800

    女仆妖精维度和群系黑名单添加

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/config/subconfig/MiscConfig.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/config/subconfig/MiscConfig.java
@@ -1,10 +1,18 @@
 package com.github.tartaricacid.touhoulittlemaid.config.subconfig;
 
+import com.google.common.collect.Lists;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biomes;
 import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.List;
 
 public final class MiscConfig {
     public static ForgeConfigSpec.DoubleValue MAID_FAIRY_POWER_POINT;
     public static ForgeConfigSpec.IntValue MAID_FAIRY_SPAWN_PROBABILITY;
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> MAID_FAIRY_BLACKLIST_BIOME;
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> MAID_FAIRY_BLACKLIST_DIMENSION;
     public static ForgeConfigSpec.DoubleValue PLAYER_DEATH_LOSS_POWER_POINT;
     public static ForgeConfigSpec.BooleanValue GIVE_SMART_SLAB;
     public static ForgeConfigSpec.DoubleValue SHRINE_LAMP_EFFECT_COST;
@@ -19,6 +27,16 @@ public final class MiscConfig {
 
         builder.comment("Maid fairy's spawn probability (zombie is 100, enderman is 10)");
         MAID_FAIRY_SPAWN_PROBABILITY = builder.defineInRange("MaidFairySpawnProbability", 70, 0, Integer.MAX_VALUE);
+
+        builder.comment("The following biome do not spawn maid fairy");
+        MAID_FAIRY_BLACKLIST_BIOME = builder.defineList("MaidFairyBlacklistBiome",
+                Lists.newArrayList(Biomes.THE_VOID.location().toString(),
+                        Biomes.MUSHROOM_FIELDS.location().toString()), MiscConfig::checkId);
+
+        builder.comment("The following dimension do not spawn maid fairy");
+        MAID_FAIRY_BLACKLIST_DIMENSION = builder.defineList("MaidFairyBlacklistDimension",
+                Lists.newArrayList(Level.NETHER.location().toString(), Level.END.location().toString(),
+                        "twilightforest:twilight_forest"), MiscConfig::checkId);
 
         builder.comment("Loss power point after player death");
         PLAYER_DEATH_LOSS_POWER_POINT = builder.defineInRange("PlayerDeathLossPowerPoint", 1.0, 0, 5);
@@ -36,5 +54,12 @@ public final class MiscConfig {
         SHRINE_LAMP_MAX_RANGE = builder.defineInRange("ShrineLampMaxRange", 6, 0, Integer.MAX_VALUE);
 
         builder.pop();
+    }
+
+    private static boolean checkId(Object o) {
+        if (o instanceof String name) {
+            return ResourceLocation.isValidResourceLocation(name);
+        }
+        return false;
     }
 }
