956f05caf3a6db3588f8647e626291b2fa14493e
tartaric_acid <baka943@qq.com>
2023-09-27 19:44:45 +0800

    添加女仆妖精生成黑名单

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/init/registry/MobSpawnInfoRegistry.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/init/registry/MobSpawnInfoRegistry.java
@@ -3,13 +3,17 @@ package com.github.tartaricacid.touhoulittlemaid.init.registry;
 import com.github.tartaricacid.touhoulittlemaid.config.subconfig.MiscConfig;
 import com.github.tartaricacid.touhoulittlemaid.init.InitEntities;
 import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import static com.github.tartaricacid.touhoulittlemaid.config.subconfig.MiscConfig.MAID_FAIRY_BLACKLIST_BIOME;
+import static com.github.tartaricacid.touhoulittlemaid.config.subconfig.MiscConfig.MAID_FAIRY_BLACKLIST_DIMENSION;
 
 @Mod.EventBusSubscriber
 public final class MobSpawnInfoRegistry {
@@ -17,17 +21,23 @@ public final class MobSpawnInfoRegistry {
 
     @SubscribeEvent
     public static void addMobSpawnInfo(LevelEvent.PotentialSpawns event) {
-        Holder<Biome> biome = event.getLevel().getBiome(event.getPos());
-        if (biomeIsOkay(biome) && event.getMobCategory() == MobCategory.MONSTER) {
-            if (SPAWNER_DATA == null) {
-                SPAWNER_DATA = new MobSpawnSettings.SpawnerData(InitEntities.FAIRY.get(), MiscConfig.MAID_FAIRY_SPAWN_PROBABILITY.get(), 2, 6);
+        if (event.getLevel() instanceof Level level) {
+            ResourceLocation dimensionId = level.dimension().location();
+            Holder<Biome> biome = level.getBiome(event.getPos());
+            if (dimensionIsOkay(dimensionId) && biomeIsOkay(biome) && event.getMobCategory() == MobCategory.MONSTER) {
+                if (SPAWNER_DATA == null) {
+                    SPAWNER_DATA = new MobSpawnSettings.SpawnerData(InitEntities.FAIRY.get(), MiscConfig.MAID_FAIRY_SPAWN_PROBABILITY.get(), 2, 6);
+                }
+                event.addSpawnerData(SPAWNER_DATA);
             }
-            event.addSpawnerData(SPAWNER_DATA);
         }
     }
 
+    private static boolean dimensionIsOkay(ResourceLocation id) {
+        return !MAID_FAIRY_BLACKLIST_DIMENSION.get().contains(id.toString());
+    }
+
     private static boolean biomeIsOkay(Holder<Biome> biome) {
-        // TODO: 2021/10/17 添加配置文件，管控可生成的生物群系
-        return !biome.is(Biomes.NETHER_WASTES) && !biome.is(Biomes.THE_END) && !biome.is(Biomes.THE_VOID) && !biome.is(Biomes.MUSHROOM_FIELDS);
+        return MAID_FAIRY_BLACKLIST_BIOME.get().stream().noneMatch(name -> biome.is(new ResourceLocation(name)));
     }
 }
