7bed5fdf2eb0c736f1aa05ec1fb488244ddc3ef2
tartaric_acid <baka943@qq.com>
2023-10-28 00:29:02 +0800

    修正 mixin 错误

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/mixin/MixinArrowEntity.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/mixin/MixinArrowEntity.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-@Mixin(value = AbstractArrow.class, remap = false)
+@Mixin(value = AbstractArrow.class)
 public interface MixinArrowEntity {
     @Accessor("inGround")
     boolean tlmInGround();
