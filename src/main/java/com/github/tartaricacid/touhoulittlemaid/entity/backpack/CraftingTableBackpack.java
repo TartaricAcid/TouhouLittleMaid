a8c422ee777fccaf939272295283d3af94415a8c
tartaric_acid <baka943@qq.com>
2023-12-12 23:53:59 +0800

    修正部分问题

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/backpack/CraftingTableBackpack.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/backpack/CraftingTableBackpack.java
@@ -21,6 +21,8 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.Nullable;
 
 public class CraftingTableBackpack extends IMaidBackpack {
@@ -67,18 +69,21 @@ public class CraftingTableBackpack extends IMaidBackpack {
     }
 
     @Override
+    @OnlyIn(Dist.CLIENT)
     public void offsetBackpackItem(PoseStack poseStack) {
         poseStack.translate(0, 0.625, 0.25);
     }
 
     @Nullable
     @Override
+    @OnlyIn(Dist.CLIENT)
     public EntityModel<EntityMaid> getBackpackModel(EntityModelSet modelSet) {
         return new CraftingTableBackpackModel(modelSet.bakeLayer(CraftingTableBackpackModel.LAYER));
     }
 
     @Nullable
     @Override
+    @OnlyIn(Dist.CLIENT)
     public ResourceLocation getBackpackTexture() {
         return new ResourceLocation(TouhouLittleMaid.MOD_ID, "textures/entity/crafting_table_backpack.png");
     }
