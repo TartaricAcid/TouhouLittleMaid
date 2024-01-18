a216e6334f7e9b9d62d98a749b4c84b6b55c3be9
tartaric_acid <baka943@qq.com>
2024-01-18 14:38:36 +0800

    修改部分内容

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/client/gui/widget/button/MaidDownloadButton.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/client/gui/widget/button/MaidDownloadButton.java
@@ -1,25 +1,16 @@
 package com.github.tartaricacid.touhoulittlemaid.client.gui.widget.button;
 
-import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.client.download.InfoGetManager;
 import com.github.tartaricacid.touhoulittlemaid.client.gui.entity.ModelDownloadGui;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.ChatFormatting;
+import com.github.tartaricacid.touhoulittlemaid.util.TipsHelper;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.FormattedCharSequence;
-import net.minecraft.util.Mth;
-
-import java.util.List;
 
 public class MaidDownloadButton extends ImageButton {
-    private static final ResourceLocation BUTTON = new ResourceLocation(TouhouLittleMaid.MOD_ID, "textures/gui/maid_gui_button.png");
-
     public MaidDownloadButton(int pX, int pY, ResourceLocation texture) {
         super(pX, pY, 41, 20, 0, 86, 20, texture, (b) -> {
             InfoGetManager.STATUE = InfoGetManager.Statue.NOT_UPDATE;
@@ -28,64 +19,7 @@ public class MaidDownloadButton extends ImageButton {
     }
 
     public void renderExtraTips(GuiGraphics graphics) {
-        graphics.pose().pushPose();
-        graphics.pose().translate(0, 0, 450);
-
-        MutableComponent text = getText();
-        if (text.equals(Component.empty())) {
-            return;
-        }
-
-        int xOffset = this.getX() + this.width / 2;
-        int yOffset = this.getY() - 22;
-        long number = (System.currentTimeMillis() / 400) % 2;
-        if (number == 1) {
-            yOffset += 1;
-        }
-
-        Font font = Minecraft.getInstance().font;
-        List<FormattedCharSequence> split = font.split(text, 150);
-        int size = split.size();
-
-        if (size == 1) {
-            int textWidth = Mth.clamp(font.width(text) + 26, 40, 150);
-            RenderSystem.enableDepthTest();
-            graphics.blit(BUTTON, xOffset, yOffset, 0, 128, 10, 20);
-            int times = (textWidth - 20) / 20;
-            int startX = xOffset + 10;
-            for (int i = 0; i < times; i++) {
-                graphics.blit(BUTTON, startX, yOffset, 10, 128, 20, 20);
-                startX = startX + 20;
-            }
-            int last = textWidth - times * 20 - 20;
-            graphics.blit(BUTTON, startX, yOffset, 10, 128, last, 20);
-            graphics.blit(BUTTON, xOffset + textWidth - 10, yOffset, 30, 128, 10, 20);
-            graphics.blit(BUTTON, xOffset + textWidth - 20, yOffset - 2, 42, 128, 16, 16);
-            if (!text.equals(Component.empty())) {
-                graphics.drawString(font, text, xOffset + 5, yOffset + 4, ChatFormatting.YELLOW.getColor(), false);
-            }
-        }
-
-        if (size >= 2) {
-            yOffset = yOffset - 10;
-            int textWidth = font.width(split.get(0)) + 26;
-            RenderSystem.enableDepthTest();
-            graphics.blit(BUTTON, xOffset, yOffset, 0, 149, 10, 30);
-            int times = (textWidth - 20) / 20;
-            int startX = xOffset + 10;
-            for (int i = 0; i < times; i++) {
-                graphics.blit(BUTTON, startX, yOffset, 10, 149, 20, 30);
-                startX = startX + 20;
-            }
-            int last = textWidth - times * 20 - 20;
-            graphics.blit(BUTTON, startX, yOffset, 10, 149, last, 30);
-            graphics.blit(BUTTON, xOffset + textWidth - 10, yOffset, 30, 149, 10, 30);
-            graphics.blit(BUTTON, xOffset + textWidth - 20, yOffset + 5, 42, 128, 16, 16);
-            graphics.drawString(font, split.get(0), xOffset + 5, yOffset + 4, ChatFormatting.YELLOW.getColor(), false);
-            graphics.drawString(font, split.get(1), xOffset + 5, yOffset + 14, ChatFormatting.YELLOW.getColor(), false);
-        }
-
-        graphics.pose().popPose();
+        TipsHelper.renderTips(graphics, this, getText());
     }
 
     private MutableComponent getText() {
---
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/util/TipsHelper.java
@@ -0,0 +1,98 @@
+package com.github.tartaricacid.touhoulittlemaid.util;
+
+import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.util.Mth;
+
+import java.util.List;
+
+public final class TipsHelper {
+    private static final ResourceLocation BUTTON = new ResourceLocation(TouhouLittleMaid.MOD_ID, "textures/gui/maid_gui_button.png");
+
+    public static void renderTips(GuiGraphics graphics, Button button, Component text) {
+        graphics.pose().pushPose();
+        graphics.pose().translate(0, 0, 450);
+
+        if (text.equals(Component.empty())) {
+            return;
+        }
+
+        int xOffset = button.getX() + button.getWidth() - 8;
+        int yOffset = button.getY() - 12;
+        long number = (System.currentTimeMillis() / 400) % 2;
+        if (number == 1) {
+            yOffset += 1;
+        }
+
+        Font font = Minecraft.getInstance().font;
+        List<FormattedCharSequence> split = font.split(text, 150);
+        int size = split.size();
+
+        if (size == 1) {
+            int textWidth = Mth.clamp(font.width(text) + 26, 40, 150);
+            RenderSystem.enableDepthTest();
+            graphics.blit(BUTTON, xOffset, yOffset, 0, 128, 10, 20);
+            int times = (textWidth - 20) / 20;
+            int startX = xOffset + 10;
+            for (int i = 0; i < times; i++) {
+                graphics.blit(BUTTON, startX, yOffset, 10, 128, 20, 20);
+                startX = startX + 20;
+            }
+            int last = textWidth - times * 20 - 20;
+            graphics.blit(BUTTON, startX, yOffset, 10, 128, last, 20);
+            graphics.blit(BUTTON, xOffset + textWidth - 10, yOffset, 30, 128, 10, 20);
+            graphics.blit(BUTTON, xOffset + textWidth - 20, yOffset - 2, 42, 128, 16, 16);
+            if (!text.equals(Component.empty())) {
+                graphics.drawString(font, text, xOffset + 5, yOffset + 4, 0XFFFF55, false);
+            }
+        }
+
+        if (size == 2) {
+            yOffset = yOffset - 10;
+            int textWidth = font.width(split.get(0)) + 26;
+            RenderSystem.enableDepthTest();
+            graphics.blit(BUTTON, xOffset, yOffset, 0, 149, 10, 30);
+            int times = (textWidth - 20) / 20;
+            int startX = xOffset + 10;
+            for (int i = 0; i < times; i++) {
+                graphics.blit(BUTTON, startX, yOffset, 10, 149, 20, 30);
+                startX = startX + 20;
+            }
+            int last = textWidth - times * 20 - 20;
+            graphics.blit(BUTTON, startX, yOffset, 10, 149, last, 30);
+            graphics.blit(BUTTON, xOffset + textWidth - 10, yOffset, 30, 149, 10, 30);
+            graphics.blit(BUTTON, xOffset + textWidth - 20, yOffset + 5, 42, 128, 16, 16);
+            graphics.drawString(font, split.get(0), xOffset + 5, yOffset + 4, 0XFFFF55, false);
+            graphics.drawString(font, split.get(1), xOffset + 5, yOffset + 14, 0XFFFF55, false);
+        }
+
+        if (size >= 3) {
+            yOffset = yOffset - 20;
+            int textWidth = font.width(split.get(0)) + 26;
+            RenderSystem.enableDepthTest();
+            graphics.blit(BUTTON, xOffset, yOffset, 0, 180, 10, 40);
+            int times = (textWidth - 20) / 20;
+            int startX = xOffset + 10;
+            for (int i = 0; i < times; i++) {
+                graphics.blit(BUTTON, startX, yOffset, 10, 180, 20, 40);
+                startX = startX + 20;
+            }
+            int last = textWidth - times * 20 - 20;
+            graphics.blit(BUTTON, startX, yOffset, 10, 180, last, 40);
+            graphics.blit(BUTTON, xOffset + textWidth - 10, yOffset, 30, 180, 10, 40);
+            graphics.blit(BUTTON, xOffset + textWidth - 20, yOffset + 10, 42, 128, 16, 16);
+            graphics.drawString(font, split.get(0), xOffset + 5, yOffset + 4, 0XFFFF55, false);
+            graphics.drawString(font, split.get(1), xOffset + 5, yOffset + 14, 0XFFFF55, false);
+            graphics.drawString(font, split.get(2), xOffset + 5, yOffset + 24, 0XFFFF55, false);
+        }
+
+        graphics.pose().popPose();
+    }
+}
