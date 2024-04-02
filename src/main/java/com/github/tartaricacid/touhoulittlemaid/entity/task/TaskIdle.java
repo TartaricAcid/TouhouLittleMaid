79da91ecad12edf1c5982ae01e401051836a5aa9
tartaric_acid <baka943@qq.com>
2024-04-02 23:38:08 +0800

    部分修正 - 睡觉时不再乱动 - 修正 IDLE 模式 AI 重复问题 - 金苹果、药水、牛奶桶现在是 Shift 右击使用了 - 修正命名牌显示问题

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/task/TaskIdle.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/task/TaskIdle.java
@@ -2,24 +2,18 @@ package com.github.tartaricacid.touhoulittlemaid.entity.task;
 
 import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.api.task.IMaidTask;
-import com.github.tartaricacid.touhoulittlemaid.entity.ai.brain.task.MaidBegTask;
-import com.github.tartaricacid.touhoulittlemaid.entity.ai.brain.task.MaidRunOne;
 import com.github.tartaricacid.touhoulittlemaid.entity.ai.brain.task.MaidSnowballTargetTask;
 import com.github.tartaricacid.touhoulittlemaid.entity.ai.brain.task.MaidStartSnowballAttacking;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.init.InitSounds;
 import com.github.tartaricacid.touhoulittlemaid.util.SoundUtil;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.behavior.BehaviorControl;
-import net.minecraft.world.entity.ai.behavior.DoNothing;
-import net.minecraft.world.entity.ai.behavior.SetEntityLookTarget;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -52,20 +46,9 @@ public class TaskIdle implements IMaidTask {
 
     @Override
     public List<Pair<Integer, BehaviorControl<? super EntityMaid>>> createBrainTasks(EntityMaid maid) {
-        Pair<BehaviorControl<? super EntityMaid>, Integer> lookToPlayer = Pair.of(SetEntityLookTarget.create(EntityType.PLAYER, 5), 1);
-        Pair<BehaviorControl<? super EntityMaid>, Integer> lookToMaid = Pair.of(SetEntityLookTarget.create(EntityMaid.TYPE, 5), 1);
-        Pair<BehaviorControl<? super EntityMaid>, Integer> lookToWolf = Pair.of(SetEntityLookTarget.create(EntityType.WOLF, 5), 1);
-        Pair<BehaviorControl<? super EntityMaid>, Integer> lookToCat = Pair.of(SetEntityLookTarget.create(EntityType.CAT, 5), 1);
-        Pair<BehaviorControl<? super EntityMaid>, Integer> lookToParrot = Pair.of(SetEntityLookTarget.create(EntityType.PARROT, 5), 1);
-        Pair<BehaviorControl<? super EntityMaid>, Integer> noLook = Pair.of(new DoNothing(20, 40), 2);
-        Pair<BehaviorControl<? super EntityMaid>, Integer> findSnowballTarget = Pair.of(new MaidStartSnowballAttacking<>(this::canSnowballFight, this::findFirstValidSnowballTarget), 2);
-        MaidRunOne randomLookTask = new MaidRunOne(ImmutableList.of(lookToPlayer, lookToMaid, lookToWolf, lookToCat, lookToParrot, findSnowballTarget, noLook));
-        MaidSnowballTargetTask snowballTargetTask = new MaidSnowballTargetTask(40);
-
-        Pair<Integer, BehaviorControl<? super EntityMaid>> beg = Pair.of(5, new MaidBegTask());
-        Pair<Integer, BehaviorControl<? super EntityMaid>> supplemented = Pair.of(6, randomLookTask);
-        Pair<Integer, BehaviorControl<? super EntityMaid>> snowballFight = Pair.of(6, snowballTargetTask);
-        return Lists.newArrayList(beg, supplemented, snowballFight);
+        Pair<Integer, BehaviorControl<? super EntityMaid>> findSnowballTarget = Pair.of(6, new MaidStartSnowballAttacking<>(this::canSnowballFight, this::findFirstValidSnowballTarget));
+        Pair<Integer, BehaviorControl<? super EntityMaid>> snowballFight = Pair.of(7, new MaidSnowballTargetTask(40));
+        return Lists.newArrayList(findSnowballTarget, snowballFight);
     }
 
     private boolean canSnowballFight(EntityMaid maid) {
--- src/main/java/com/github/tartaricacid/touhoulittlemaid/event/maid/ApplyGoldenAppleEvent.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/event/maid/ApplyGoldenAppleEvent.java
@@ -3,6 +3,7 @@ package com.github.tartaricacid.touhoulittlemaid.event.maid;
 
 import com.github.tartaricacid.touhoulittlemaid.api.event.InteractMaidEvent;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.ItemStack;
@@ -18,8 +19,9 @@ public final class ApplyGoldenAppleEvent {
         EntityMaid maid = event.getMaid();
         Level world = event.getWorld();
         FoodProperties food = stack.getItem().getFoodProperties(stack, maid);
+        Player player = event.getPlayer();
 
-        if (food == Foods.GOLDEN_APPLE || food == Foods.ENCHANTED_GOLDEN_APPLE) {
+        if (player.isDiscrete() && (food == Foods.GOLDEN_APPLE || food == Foods.ENCHANTED_GOLDEN_APPLE)) {
             maid.eat(world, stack);
             event.setCanceled(true);
         }
