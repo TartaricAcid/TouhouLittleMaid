ba101696ef4477d1f7d5efd308a6a3feb699c0e8
unknown <617510630@qq.com>
2024-05-23 23:17:55 +0800

    SlashBlade:Resharped compat

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/slashblade/SlashBladeCompat.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/slashblade/SlashBladeCompat.java
@@ -3,24 +3,22 @@ package com.github.tartaricacid.touhoulittlemaid.compat.slashblade;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.entity.task.TaskAttack;
 import mods.flammpfeil.slashblade.capability.slashblade.CapabilitySlashBlade;
+import mods.flammpfeil.slashblade.item.ItemSlashBlade;
 import mods.flammpfeil.slashblade.util.AttackManager;
 import mods.flammpfeil.slashblade.util.KnockBacks;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class SlashBladeCompat {
     private static final String SLASH_BLADE = "slashblade";
-    private static final ResourceLocation SLASH_BLADE_ID = new ResourceLocation(SLASH_BLADE, "slashblade");
 
     public static boolean isSlashBladeLoaded() {
         return ModList.get().isLoaded(SLASH_BLADE);
     }
 
     public static boolean isSlashBladeItem(ItemStack stack) {
-        return isSlashBladeLoaded() && SLASH_BLADE_ID.equals(ForgeRegistries.ITEMS.getKey(stack.getItem()));
+        return isSlashBladeLoaded() && stack.getItem() instanceof ItemSlashBlade;
     }
 
     public static void swingSlashBlade(EntityMaid maid, ItemStack itemInHand) {
