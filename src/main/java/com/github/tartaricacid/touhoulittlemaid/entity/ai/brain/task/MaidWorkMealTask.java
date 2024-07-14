77d0de8e277155a0788950940f6b346785cd983b
Azumic <775300932@qq.com>
2024-07-14 13:16:08 +0800

    maid heal self and work meal

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/ai/brain/task/MaidWorkMealTask.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/ai/brain/task/MaidWorkMealTask.java
@@ -10,6 +10,7 @@ import com.google.common.collect.ImmutableMap;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.wrapper.RangedWrapper;
 
 import java.util.List;
@@ -35,34 +36,61 @@ public class MaidWorkMealTask extends MaidCheckRateTask {
     @Override
     protected void start(ServerLevel serverLevel, EntityMaid maid, long gameTime) {
         List<IMaidMeal> maidMeals = MaidMealManager.getMaidMeals(MaidMealType.WORK_MEAL);
+
+        // 先查询手部的物品能否吃：能就直接开吃，否就进行后续工作
         for (InteractionHand hand : InteractionHand.values()) {
             ItemStack itemInHand = maid.getItemInHand(hand);
 
-            emptyItemCheck:
             if (itemInHand.isEmpty()) {
-                // 尝试在背包中寻找食物放入
-                RangedWrapper backpackInv = maid.getAvailableBackpackInv();
-                for (int i = 0; i < backpackInv.getSlots(); i++) {
-                    ItemStack stack = backpackInv.getStackInSlot(i);
-                    if (stack.isEmpty()) {
-                        continue;
-                    }
-                    for (IMaidMeal maidMeal : maidMeals) {
-                        if (maidMeal.canMaidEat(maid, stack, hand)) {
-                            maid.setItemInHand(hand, stack.copy());
-                            backpackInv.setStackInSlot(i, ItemStack.EMPTY);
-                            itemInHand = maid.getItemInHand(hand);
-                            break emptyItemCheck;
-                        }
-                    }
-                }
-                // 搜索完所有的物品都没有合适的，就不执行后续内容了
                 continue;
             }
 
             for (IMaidMeal maidMeal : maidMeals) {
                 if (maidMeal.canMaidEat(maid, itemInHand, hand)) {
                     maidMeal.onMaidEat(maid, itemInHand, hand);
+                    return;
+                }
+            }
+        }
+
+        // 对手部进行处理：如果没有空的手部，那就取副手
+        InteractionHand eanHand = InteractionHand.OFF_HAND;
+        for (InteractionHand hand : InteractionHand.values()) {
+            if (maid.getItemInHand(hand).isEmpty()) {
+                eanHand = hand;
+                break;
+            }
+        }
+        ItemStack itemInHand = maid.getItemInHand(eanHand);
+
+        // 尝试在背包中寻找食物放入
+        boolean hasFood = false;
+        RangedWrapper backpackInv = maid.getAvailableBackpackInv();
+        emptyItemCheck:
+        for (int i = 0; i < backpackInv.getSlots(); i++) {
+            ItemStack stack = backpackInv.getStackInSlot(i);
+            if (stack.isEmpty()) {
+                continue;
+            }
+            for (IMaidMeal maidMeal : maidMeals) {
+                if (maidMeal.canMaidEat(maid, stack, eanHand)) {
+                    ItemStack foodStack = stack.copy();
+                    ItemStack handStack = itemInHand.copy();
+                    maid.setItemInHand(eanHand, foodStack);
+                    backpackInv.setStackInSlot(i, ItemStack.EMPTY);
+                    ItemHandlerHelper.insertItemStacked(backpackInv, handStack, false);
+                    itemInHand = maid.getItemInHand(eanHand);
+                    hasFood = true;
+                    break emptyItemCheck;
+                }
+            }
+        }
+
+        // 开吃
+        if (hasFood) {
+            for (IMaidMeal maidMeal : maidMeals) {
+                if (maidMeal.canMaidEat(maid, itemInHand, eanHand)) {
+                    maidMeal.onMaidEat(maid, itemInHand, eanHand);
                     return;
                 }
             }
