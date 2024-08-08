60458689b583854bdc2b0d336199471078becaf7
AnonymeMeow <146799252+AnonymeMeow@users.noreply.github.com>
2024-08-09 06:11:38 +0800

    wip

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/backpack/data/FurnaceBackpackData.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/backpack/data/FurnaceBackpackData.java
@@ -148,7 +148,7 @@ public class FurnaceBackpackData extends SimpleContainer implements IBackpackDat
     @Override
     public void setItem(int index, ItemStack stack) {
         ItemStack slotItem = this.getItem(index);
-        boolean isSameItem = !stack.isEmpty() && ItemStack.isSameItemSameTags(slotItem, stack);
+        boolean isSameItem = !stack.isEmpty() && ItemStack.isSameItemSameComponents(slotItem, stack);
         super.setItem(index, stack);
         if (index == 0 && !isSameItem) {
             this.cookingTotalTime = getTotalCookTime(this.level);
