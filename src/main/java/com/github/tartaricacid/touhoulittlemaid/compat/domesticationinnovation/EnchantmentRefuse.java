0b0a059d2ec67b04f0a0babb14021823153bc949
Crystal1921 <1921331938@qq.com>
2024-08-10 22:39:32 +0800

    注释掉compat部分

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/domesticationinnovation/EnchantmentRefuse.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/domesticationinnovation/EnchantmentRefuse.java
@@ -1,55 +1,55 @@
-package com.github.tartaricacid.touhoulittlemaid.compat.domesticationinnovation;
-
-import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.neoforged.bus.api.EventPriority;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModList;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-
-import java.util.Map;
-
-@EventBusSubscriber
-public class EnchantmentRefuse {
-    private static final String DOMESTICATION_INNOVATION = "domesticationinnovation";
-    private static final ResourceLocation COLLAR_TAG_ID = ResourceLocation.fromNamespaceAndPath(DOMESTICATION_INNOVATION, "collar_tag");
-    private static final ResourceLocation UNDEAD_CURSE_ENCHANTMENT_ID = ResourceLocation.fromNamespaceAndPath(DOMESTICATION_INNOVATION, "undead_curse");
-
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void onInteractMaid(PlayerInteractEvent.EntityInteract event) {
-        if (!ModList.get().isLoaded(DOMESTICATION_INNOVATION)) {
-            return;
-        }
-        if (!(event.getTarget() instanceof EntityMaid maid)) {
-            return;
-        }
-        if (maid.level.isClientSide) {
-            return;
-        }
-        Player player = event.getEntity();
-        if (!maid.isOwnedBy(player)) {
-            return;
-        }
-        ItemStack stack = event.getItemStack();
-        ResourceLocation itemsKey = BuiltInRegistries.ITEM.getKey(stack.getItem());
-        if (!COLLAR_TAG_ID.equals(itemsKey)) {
-            return;
-        }
-        Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(stack);
-        for (Enchantment enchantment : enchantments.keySet()) {
-            ResourceLocation enchantmentsKey = BuiltInRegistries.ENCHANTMENT.getKey(enchantment);
-            if (UNDEAD_CURSE_ENCHANTMENT_ID.equals(enchantmentsKey)) {
-                event.setCanceled(true);
-                player.sendSystemMessage(Component.translatable("domesticationinnovation.touhou_little_maid.enchantment.undead_curse"));
-                return;
-            }
-        }
-    }
-}
+//package com.github.tartaricacid.touhoulittlemaid.compat.domesticationinnovation;
+//
+//import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+//import net.minecraft.core.registries.BuiltInRegistries;
+//import net.minecraft.network.chat.Component;
+//import net.minecraft.resources.ResourceLocation;
+//import net.minecraft.world.entity.player.Player;
+//import net.minecraft.world.item.ItemStack;
+//import net.minecraft.world.item.enchantment.Enchantment;
+//import net.minecraft.world.item.enchantment.EnchantmentHelper;
+//import net.neoforged.bus.api.EventPriority;
+//import net.neoforged.bus.api.SubscribeEvent;
+//import net.neoforged.fml.ModList;
+//import net.neoforged.fml.common.EventBusSubscriber;
+//import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+//
+//import java.util.Map;
+//
+//@EventBusSubscriber
+//public class EnchantmentRefuse {
+//    private static final String DOMESTICATION_INNOVATION = "domesticationinnovation";
+//    private static final ResourceLocation COLLAR_TAG_ID = ResourceLocation.fromNamespaceAndPath(DOMESTICATION_INNOVATION, "collar_tag");
+//    private static final ResourceLocation UNDEAD_CURSE_ENCHANTMENT_ID = ResourceLocation.fromNamespaceAndPath(DOMESTICATION_INNOVATION, "undead_curse");
+//
+//    @SubscribeEvent(priority = EventPriority.HIGHEST)
+//    public static void onInteractMaid(PlayerInteractEvent.EntityInteract event) {
+//        if (!ModList.get().isLoaded(DOMESTICATION_INNOVATION)) {
+//            return;
+//        }
+//        if (!(event.getTarget() instanceof EntityMaid maid)) {
+//            return;
+//        }
+//        if (maid.level.isClientSide) {
+//            return;
+//        }
+//        Player player = event.getEntity();
+//        if (!maid.isOwnedBy(player)) {
+//            return;
+//        }
+//        ItemStack stack = event.getItemStack();
+//        ResourceLocation itemsKey = BuiltInRegistries.ITEM.getKey(stack.getItem());
+//        if (!COLLAR_TAG_ID.equals(itemsKey)) {
+//            return;
+//        }
+//        Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(stack);
+//        for (Enchantment enchantment : enchantments.keySet()) {
+//            ResourceLocation enchantmentsKey = BuiltInRegistries.ENCHANTMENT.getKey(enchantment);
+//            if (UNDEAD_CURSE_ENCHANTMENT_ID.equals(enchantmentsKey)) {
+//                event.setCanceled(true);
+//                player.sendSystemMessage(Component.translatable("domesticationinnovation.touhou_little_maid.enchantment.undead_curse"));
+//                return;
+//            }
+//        }
+//    }
+//}
--- src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/tacz/task/TaskGunAttack.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/tacz/task/TaskGunAttack.java
@@ -1,82 +1,82 @@
-package com.github.tartaricacid.touhoulittlemaid.compat.tacz.task;
-
-import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
-import com.github.tartaricacid.touhoulittlemaid.api.task.IAttackTask;
-import com.github.tartaricacid.touhoulittlemaid.compat.tacz.ai.GunAttackStrafingTask;
-import com.github.tartaricacid.touhoulittlemaid.compat.tacz.ai.GunShootTargetTask;
-import com.github.tartaricacid.touhoulittlemaid.compat.tacz.ai.GunWalkToTarget;
-import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
-import com.github.tartaricacid.touhoulittlemaid.init.InitSounds;
-import com.github.tartaricacid.touhoulittlemaid.util.SoundUtil;
-import com.google.common.collect.Lists;
-import com.mojang.datafixers.util.Pair;
-import com.tacz.guns.GunMod;
-import com.tacz.guns.api.item.IGun;
-import com.tacz.guns.api.item.builder.GunItemBuilder;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.behavior.BehaviorControl;
-import net.minecraft.world.entity.ai.behavior.StartAttacking;
-import net.minecraft.world.entity.ai.behavior.StopAttackingIfTargetInvalid;
-import net.minecraft.world.item.ItemStack;
-
-import javax.annotation.Nullable;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Predicate;
-
-public class TaskGunAttack implements IAttackTask {
-    public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(TouhouLittleMaid.MOD_ID, "gun_attack");
-
-    @Override
-    public ResourceLocation getUid() {
-        return UID;
-    }
-
-    @Override
-    public ItemStack getIcon() {
-        return GunItemBuilder.create().setId(ResourceLocation.fromNamespaceAndPath(GunMod.MOD_ID, "glock_17")).build();
-    }
-
-    @Nullable
-    @Override
-    public SoundEvent getAmbientSound(EntityMaid maid) {
-        return SoundUtil.attackSound(maid, InitSounds.MAID_RANGE_ATTACK.get(), 0.5f);
-    }
-
-    @Override
-    public boolean enableLookAndRandomWalk(EntityMaid maid) {
-        return false;
-    }
-
-    @Override
-    public List<Pair<Integer, BehaviorControl<? super EntityMaid>>> createBrainTasks(EntityMaid maid) {
-        BehaviorControl<EntityMaid> supplementedTask = StartAttacking.create(this::mainhandHoldGun, IAttackTask::findFirstValidAttackTarget);
-        BehaviorControl<EntityMaid> findTargetTask = StopAttackingIfTargetInvalid.create(target -> !mainhandHoldGun(maid) || farAway(target, maid));
-        BehaviorControl<EntityMaid> gunWalkTargetTask = GunWalkToTarget.create(0.6f);
-        BehaviorControl<EntityMaid> gunAttackStrafingTask = new GunAttackStrafingTask();
-        BehaviorControl<EntityMaid> gunShootTargetTask = new GunShootTargetTask();
-
-        return Lists.newArrayList(
-                Pair.of(5, supplementedTask),
-                Pair.of(5, findTargetTask),
-                Pair.of(5, gunWalkTargetTask),
-                Pair.of(5, gunAttackStrafingTask),
-                Pair.of(5, gunShootTargetTask)
-        );
-    }
-
-    @Override
-    public List<Pair<String, Predicate<EntityMaid>>> getConditionDescription(EntityMaid maid) {
-        return Collections.singletonList(Pair.of("has_tacz_gun", this::mainhandHoldGun));
-    }
-
-    private boolean mainhandHoldGun(EntityMaid maid) {
-        return IGun.mainhandHoldGun(maid);
-    }
-
-    private boolean farAway(LivingEntity target, EntityMaid maid) {
-        return maid.distanceTo(target) > maid.getRestrictRadius();
-    }
-}
+//package com.github.tartaricacid.touhoulittlemaid.compat.tacz.task;
+//
+//import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
+//import com.github.tartaricacid.touhoulittlemaid.api.task.IAttackTask;
+//import com.github.tartaricacid.touhoulittlemaid.compat.tacz.ai.GunAttackStrafingTask;
+//import com.github.tartaricacid.touhoulittlemaid.compat.tacz.ai.GunShootTargetTask;
+//import com.github.tartaricacid.touhoulittlemaid.compat.tacz.ai.GunWalkToTarget;
+//import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+//import com.github.tartaricacid.touhoulittlemaid.init.InitSounds;
+//import com.github.tartaricacid.touhoulittlemaid.util.SoundUtil;
+//import com.google.common.collect.Lists;
+//import com.mojang.datafixers.util.Pair;
+//import com.tacz.guns.GunMod;
+//import com.tacz.guns.api.item.IGun;
+//import com.tacz.guns.api.item.builder.GunItemBuilder;
+//import net.minecraft.resources.ResourceLocation;
+//import net.minecraft.sounds.SoundEvent;
+//import net.minecraft.world.entity.LivingEntity;
+//import net.minecraft.world.entity.ai.behavior.BehaviorControl;
+//import net.minecraft.world.entity.ai.behavior.StartAttacking;
+//import net.minecraft.world.entity.ai.behavior.StopAttackingIfTargetInvalid;
+//import net.minecraft.world.item.ItemStack;
+//
+//import javax.annotation.Nullable;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.function.Predicate;
+//
+//public class TaskGunAttack implements IAttackTask {
+//    public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(TouhouLittleMaid.MOD_ID, "gun_attack");
+//
+//    @Override
+//    public ResourceLocation getUid() {
+//        return UID;
+//    }
+//
+//    @Override
+//    public ItemStack getIcon() {
+//        return GunItemBuilder.create().setId(ResourceLocation.fromNamespaceAndPath(GunMod.MOD_ID, "glock_17")).build();
+//    }
+//
+//    @Nullable
+//    @Override
+//    public SoundEvent getAmbientSound(EntityMaid maid) {
+//        return SoundUtil.attackSound(maid, InitSounds.MAID_RANGE_ATTACK.get(), 0.5f);
+//    }
+//
+//    @Override
+//    public boolean enableLookAndRandomWalk(EntityMaid maid) {
+//        return false;
+//    }
+//
+//    @Override
+//    public List<Pair<Integer, BehaviorControl<? super EntityMaid>>> createBrainTasks(EntityMaid maid) {
+//        BehaviorControl<EntityMaid> supplementedTask = StartAttacking.create(this::mainhandHoldGun, IAttackTask::findFirstValidAttackTarget);
+//        BehaviorControl<EntityMaid> findTargetTask = StopAttackingIfTargetInvalid.create(target -> !mainhandHoldGun(maid) || farAway(target, maid));
+//        BehaviorControl<EntityMaid> gunWalkTargetTask = GunWalkToTarget.create(0.6f);
+//        BehaviorControl<EntityMaid> gunAttackStrafingTask = new GunAttackStrafingTask();
+//        BehaviorControl<EntityMaid> gunShootTargetTask = new GunShootTargetTask();
+//
+//        return Lists.newArrayList(
+//                Pair.of(5, supplementedTask),
+//                Pair.of(5, findTargetTask),
+//                Pair.of(5, gunWalkTargetTask),
+//                Pair.of(5, gunAttackStrafingTask),
+//                Pair.of(5, gunShootTargetTask)
+//        );
+//    }
+//
+//    @Override
+//    public List<Pair<String, Predicate<EntityMaid>>> getConditionDescription(EntityMaid maid) {
+//        return Collections.singletonList(Pair.of("has_tacz_gun", this::mainhandHoldGun));
+//    }
+//
+//    private boolean mainhandHoldGun(EntityMaid maid) {
+//        return IGun.mainhandHoldGun(maid);
+//    }
+//
+//    private boolean farAway(LivingEntity target, EntityMaid maid) {
+//        return maid.distanceTo(target) > maid.getRestrictRadius();
+//    }
+//}
