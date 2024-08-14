dac4bbbb4cf6dce39f72042924b85e0d9f9cd15d
Crystal1921 <1921331938@qq.com>
2024-08-15 01:03:42 +0800

    完成不规则配方（其他实体待做）

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/crafting/AltarRecipe.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/crafting/AltarRecipe.java
@@ -1,28 +1,27 @@
 package com.github.tartaricacid.touhoulittlemaid.crafting;
 
 import com.github.tartaricacid.touhoulittlemaid.init.InitRecipes;
-import com.github.tartaricacid.touhoulittlemaid.inventory.AltarRecipeInventory;
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class AltarRecipe extends ShapedRecipe {
     String group;
@@ -30,6 +29,7 @@ public class AltarRecipe extends ShapedRecipe {
     float power;
     ItemStack result;
     ResourceLocation entityType;
+
     public AltarRecipe(String pGroup,
                        CraftingBookCategory pCategory,
                        float power,
@@ -57,11 +57,24 @@ public class AltarRecipe extends ShapedRecipe {
     }
 
     public void spawnOutputEntity(ServerLevel world, BlockPos pos, @Nullable List<ItemStack> list) {
-
+        EntityType type = BuiltInRegistries.ENTITY_TYPE.get(entityType);
+        if (type == EntityType.ITEM) {
+            ItemEntity itemEntity = new ItemEntity(world,pos.getX(),pos.getY(),pos.getZ(), this.result);
+            world.addFreshEntity(itemEntity);
+        }
     }
 
     public boolean matches(CraftingInput pInput, Level pLevel) {
-        return this.pattern.matches(pInput);
+        List<Item> list = pInput.items().stream().filter(itemStack -> !itemStack.isEmpty()).map(ItemStack::getItem).toList();
+        // 如果配方所需的材料数量与输入数量不一致，立即返回 false
+        if (this.getIngredients().size() != list.size()) {
+            return false;
+        }
+
+        for ( Ingredient ingredient : this.getIngredients()) {
+            if (Arrays.stream(ingredient.getItems()).noneMatch(itemStack -> list.contains(itemStack.getItem()))) return false;
+        }
+        return true;
     }
 
     @Override
@@ -94,7 +107,7 @@ public class AltarRecipe extends ShapedRecipe {
             ShapedRecipePattern shapedrecipepattern = ShapedRecipePattern.STREAM_CODEC.decode(friendlyByteBuf);
             ItemStack itemstack = ItemStack.STREAM_CODEC.decode(friendlyByteBuf);
             ResourceLocation entityType = friendlyByteBuf.readResourceLocation();
-            return new AltarRecipe(s, craftingbookcategory,power, shapedrecipepattern, itemstack, entityType);
+            return new AltarRecipe(s, craftingbookcategory, power, shapedrecipepattern, itemstack, entityType);
         }
 
         private void toNetwork(RegistryFriendlyByteBuf friendlyByteBuf, AltarRecipe altarRecipe) {
@@ -113,7 +126,7 @@ public class AltarRecipe extends ShapedRecipe {
 
         @Override
         public StreamCodec<RegistryFriendlyByteBuf, AltarRecipe> streamCodec() {
-            return StreamCodec.of(this::toNetwork,this::fromNetwork);
+            return StreamCodec.of(this::toNetwork, this::fromNetwork);
         }
     }
 }
