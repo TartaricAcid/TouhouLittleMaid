a5f96b840f5af81fb91f0b81e4a33b2864d22cf1
Yongqi-Zhuo <Yongqi-Zhuo@users.noreply.github.com>
2024-08-16 02:46:56 +0800

    Use all_of instead for loot table conditions.

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/dataGen/GlobalLootModifier.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/dataGen/GlobalLootModifier.java
@@ -3,6 +3,7 @@ package com.github.tartaricacid.touhoulittlemaid.dataGen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.predicates.AnyOfCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.neoforged.neoforge.common.data.GlobalLootModifierProvider;
@@ -18,14 +19,16 @@ public class GlobalLootModifier extends GlobalLootModifierProvider {
 
     @Override
     public void start() {
+        var condBuilder = new AnyOfCondition.Builder();
         BuiltInLootTables.all().stream()
                         .filter(lootTable -> lootTable.location().getPath().startsWith("chests/"))
-                        .forEach(lootTable -> add("amendments/" + lootTable.location().getPath(), new AddTableLootModifier(
-                                new LootItemCondition[]{
-                                        LootTableIdCondition.builder(lootTable.location()).build(),
-                                        LootItemRandomChanceCondition.randomChance(0.05F).build(),
-                                },
-                                LootTableGenerator.ADDITIONAL_LOOT_TABLE
-                        )));
+                        .forEach(lootTable -> condBuilder.or(LootTableIdCondition.builder(lootTable.location())));
+        add("chests_amendments", new AddTableLootModifier(
+                new LootItemCondition[]{
+                        condBuilder.build(),
+                        LootItemRandomChanceCondition.randomChance(0.05F).build(),
+                },
+                LootTableGenerator.ADDITIONAL_LOOT_TABLE
+        ));
     }
 }
