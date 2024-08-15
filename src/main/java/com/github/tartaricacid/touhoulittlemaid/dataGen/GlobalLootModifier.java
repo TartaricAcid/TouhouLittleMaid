ad1a6a19278c7388a358f136bf654de5081738e6
AnonymeMeow <146799252+AnonymeMeow@users.noreply.github.com>
2024-08-16 01:06:50 +0800

    fix loot modifier

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/dataGen/GlobalLootModifier.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/dataGen/GlobalLootModifier.java
@@ -1,20 +1,25 @@
 package com.github.tartaricacid.touhoulittlemaid.dataGen;
 
-import com.github.tartaricacid.touhoulittlemaid.loot.AdditionLootModifier;
+import com.github.tartaricacid.touhoulittlemaid.loot.AdditionalLootModifier;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.neoforged.neoforge.common.data.GlobalLootModifierProvider;
 
 import java.util.concurrent.CompletableFuture;
 
 public class GlobalLootModifier extends GlobalLootModifierProvider {
-    public GlobalLootModifier(PackOutput output, CompletableFuture<HolderLookup.Provider> registries, String modid) {
-        super(output, registries, modid);
+    public GlobalLootModifier(PackOutput output, CompletableFuture<HolderLookup.Provider> registries, String modId) {
+        super(output, registries, modId);
     }
 
     @Override
-    protected void start() {
-        add("touhou_additional", new AdditionLootModifier(new LootItemCondition[]{}));
+    public void start() {
+        add("additional", new AdditionalLootModifier(
+                new LootItemCondition[]{LootItemRandomChanceCondition.randomChance(0.05F).build()},
+                LootContextParamSets.CHEST,
+                LootTableGenerator.ADDITIONAL_LOOT_TABLE.location()));
     }
 }
--- src/main/java/com/github/tartaricacid/touhoulittlemaid/init/InitLootModifier.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/init/InitLootModifier.java
@@ -1,14 +1,15 @@
 package com.github.tartaricacid.touhoulittlemaid.init;
 
 import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
-import com.github.tartaricacid.touhoulittlemaid.loot.AdditionLootModifier;
+import com.github.tartaricacid.touhoulittlemaid.loot.AdditionalLootModifier;
 import com.mojang.serialization.MapCodec;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
+import java.util.function.Supplier;
+
 public class InitLootModifier {
-    public static final DeferredRegister<MapCodec<? extends IGlobalLootModifier>> LOOT_MODIFIERS = DeferredRegister.create(NeoForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, TouhouLittleMaid.MOD_ID);
-    public static final DeferredHolder<MapCodec<? extends IGlobalLootModifier>, MapCodec<AdditionLootModifier>> ADDITIONAL = LOOT_MODIFIERS.register("additional", () -> AdditionLootModifier.CODEC);
+    public static final DeferredRegister<MapCodec<? extends IGlobalLootModifier>> GLOBAL_LOOT_MODIFIER_SERIALIZER = DeferredRegister.create(NeoForgeRegistries.GLOBAL_LOOT_MODIFIER_SERIALIZERS, TouhouLittleMaid.MOD_ID);
+    public static final Supplier<MapCodec<? extends IGlobalLootModifier>> ADDITIONAL = GLOBAL_LOOT_MODIFIER_SERIALIZER.register("additional", () -> AdditionalLootModifier.CODEC);
 }
