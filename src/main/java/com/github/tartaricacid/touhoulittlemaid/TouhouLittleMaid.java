5b227eac451d8b5087573cb26d0584aded2f9796
tartaric_acid <baka943@qq.com>
2024-08-22 10:27:11 +0800

    Revert "Maybe the extra client side @Mod is unnecessary?"
    This reverts commit 15ec0e23dd0640ebecc6e5385305937bbe57cf36.

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/TouhouLittleMaid.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/TouhouLittleMaid.java
@@ -2,8 +2,6 @@ package com.github.tartaricacid.touhoulittlemaid;
 
 import com.github.tartaricacid.touhoulittlemaid.api.ILittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.block.multiblock.MultiBlockManager;
-import com.github.tartaricacid.touhoulittlemaid.client.entity.GeckoMaidEntity;
-import com.github.tartaricacid.touhoulittlemaid.compat.cloth.MenuIntegration;
 import com.github.tartaricacid.touhoulittlemaid.config.GeneralConfig;
 import com.github.tartaricacid.touhoulittlemaid.entity.backpack.BackpackManager;
 import com.github.tartaricacid.touhoulittlemaid.entity.chatbubble.ChatBubbleManger;
@@ -11,23 +9,15 @@ import com.github.tartaricacid.touhoulittlemaid.entity.task.TaskManager;
 import com.github.tartaricacid.touhoulittlemaid.entity.task.meal.MaidMealManager;
 import com.github.tartaricacid.touhoulittlemaid.init.*;
 import com.github.tartaricacid.touhoulittlemaid.init.registry.CommandRegistry;
-import com.github.tartaricacid.touhoulittlemaid.init.registry.CompatRegistry;
 import com.github.tartaricacid.touhoulittlemaid.inventory.chest.ChestManager;
 import com.github.tartaricacid.touhoulittlemaid.item.bauble.BaubleManager;
 import com.github.tartaricacid.touhoulittlemaid.network.NewNetwork;
 import com.github.tartaricacid.touhoulittlemaid.util.AnnotatedInstanceUtil;
 import com.google.common.collect.Lists;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.fml.loading.LoadingModList;
-import net.neoforged.fml.loading.moddiscovery.ModFileInfo;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -45,10 +35,6 @@ public final class TouhouLittleMaid {
         ChatBubbleManger.initDefaultChat();
         registerConfiguration(modContainer);
         modApiInit();
-        if (FMLEnvironment.dist == Dist.CLIENT) {
-            registerClientOnly();
-            registerConfigMenu(modContainer);
-        }
     }
 
     private static void initRegister(IEventBus eventBus) {
@@ -90,21 +76,5 @@ public final class TouhouLittleMaid {
         MultiBlockManager.init();
         ChestManager.init();
         MaidMealManager.init();
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    private void registerClientOnly() {
-        // 这个仅用于客户端，所以不需要在服务端注册
-        InitDataAttachment.ATTACHMENT_TYPES.register("gecko_maid", () -> GeckoMaidEntity.TYPE);
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    private void registerConfigMenu(ModContainer modContainer) {
-        ModFileInfo clothConfigInfo = LoadingModList.get().getModFileById(CompatRegistry.CLOTH_CONFIG);
-        if (clothConfigInfo != null) {
-            MenuIntegration.registerModsPage(modContainer);
-        } else {
-            modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-        }
     }
 }
