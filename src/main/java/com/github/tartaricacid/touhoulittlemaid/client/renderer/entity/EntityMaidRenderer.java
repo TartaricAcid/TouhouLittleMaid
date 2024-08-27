d5b04b6ece5430adeef8492f5af594e25a17b1c1
tartaric_acid <baka943@qq.com>
2024-08-27 17:30:00 +0800

    修正对其他模组渲染的兼容

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/client/renderer/entity/EntityMaidRenderer.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/client/renderer/entity/EntityMaidRenderer.java
@@ -48,7 +48,7 @@ public class EntityMaidRenderer extends MobRenderer<Mob, BedrockModel<Mob>> {
 
     @Override
     public void render(Mob entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource bufferIn, int packedLightIn) {
-        var maid = IMaid.convertToMaid(entity);
+        var maid = IMaid.convert(entity);
         if (maid == null) {
             return;
         }
@@ -81,8 +81,8 @@ public class EntityMaidRenderer extends MobRenderer<Mob, BedrockModel<Mob>> {
 
         // GeckoLib 接管渲染
         if (this.mainInfo.isGeckoModel()) {
-            this.geckoEntityMaidRenderer.getAnimatableEntity(maid).setMaidInfo(this.mainInfo);
-            this.geckoEntityMaidRenderer.render(maid, entityYaw, partialTicks, poseStack, bufferIn, packedLightIn);
+            this.geckoEntityMaidRenderer.getAnimatableEntity(entity).setMaidInfo(this.mainInfo);
+            this.geckoEntityMaidRenderer.render(entity, entityYaw, partialTicks, poseStack, bufferIn, packedLightIn);
             return;
         }
         // 模型动画设置
