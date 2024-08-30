2b44c33b3b8f3c0ed2318d0ded4000adcef0d378
tartaric_acid <baka943@qq.com>
2024-08-30 11:02:45 +0800

    修正服务端模型包下载功能

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/client/event/ClientPackDownloadEvent.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/client/event/ClientPackDownloadEvent.java
@@ -2,16 +2,27 @@ package com.github.tartaricacid.touhoulittlemaid.client.event;
 
 import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.client.download.ClientPackDownloadManager;
-import com.github.tartaricacid.touhoulittlemaid.client.resource.CustomPackLoader;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
-import net.neoforged.fml.loading.FMLEnvironment;
 
+@OnlyIn(Dist.CLIENT)
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = TouhouLittleMaid.MOD_ID)
 public class ClientPackDownloadEvent {
     private static final String CONFIG_NAME = "touhou_little_maid-server.toml";
+
+    /**
+     * 客户端启动并读取配置时，会触发此事件
+     */
+    @SubscribeEvent
+    public static void onLoadingConfig(ModConfigEvent.Loading event) {
+        String fileName = event.getConfig().getFileName();
+        if (CONFIG_NAME.equals(fileName)) {
+            ClientPackDownloadManager.downloadClientPack();
+        }
+    }
 
     /**
      * 玩家进入服务端，或者服务端自动重置配置时，会触发此方法
@@ -19,10 +30,7 @@ public class ClientPackDownloadEvent {
     @SubscribeEvent
     public static void onReloadingConfig(ModConfigEvent.Reloading event) {
         String fileName = event.getConfig().getFileName();
-        if (FMLEnvironment.dist == Dist.CLIENT && CONFIG_NAME.equals(fileName)) {
-            // 先清理，把服务端文件从内存中全清干净
-            CustomPackLoader.reloadPacks();
-            // 然后尝试加载服务端需要下载的文件
+        if (CONFIG_NAME.equals(fileName)) {
             ClientPackDownloadManager.downloadClientPack();
         }
     }
