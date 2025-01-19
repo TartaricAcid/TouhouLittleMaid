45d696a82dd81172b8ee433691fc0b3dbeacadd4
tartaric_acid <baka943@qq.com>
2025-01-19 19:48:45 +0800

    补充部分测试内容

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/debug/MaidDebugCommand.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/debug/MaidDebugCommand.java
@@ -11,7 +11,12 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.FarmBlock;
 
 public final class MaidDebugCommand {
     private static final String MAID_DEBUG_NAME = "debug";
@@ -20,12 +25,23 @@ public final class MaidDebugCommand {
     private static final String COUNT_NAME = "count";
     private static final String MODEL_ID = "model_id";
 
+    private static final String SET_FARM = "set_farm";
+    private static final String SIZE = "size";
+
     public static LiteralArgumentBuilder<CommandSourceStack> get() {
         LiteralArgumentBuilder<CommandSourceStack> debug = Commands.literal(MAID_DEBUG_NAME);
+
+        // 批量生成女仆
         LiteralArgumentBuilder<CommandSourceStack> spawnMaid = Commands.literal(SPAWN_MAID);
         RequiredArgumentBuilder<CommandSourceStack, String> modelId = Commands.argument(MODEL_ID, StringArgumentType.string());
         RequiredArgumentBuilder<CommandSourceStack, Integer> count = Commands.argument(COUNT_NAME, IntegerArgumentType.integer(0));
         debug.then(spawnMaid.then(modelId.then(count.executes(MaidDebugCommand::spawnMaid))));
+
+        // 生成大量农田
+        LiteralArgumentBuilder<CommandSourceStack> setFarm = Commands.literal(SET_FARM);
+        RequiredArgumentBuilder<CommandSourceStack, Integer> size = Commands.argument(SIZE, IntegerArgumentType.integer());
+        debug.then(setFarm.then(size.executes(MaidDebugCommand::setFarm)));
+
         return debug;
     }
 
@@ -46,6 +62,32 @@ public final class MaidDebugCommand {
                 serverPlayer.level.addFreshEntity(entityMaid);
             }
         }
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int setFarm(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        int size = IntegerArgumentType.getInteger(context, SIZE);
+        ServerPlayer serverPlayer = context.getSource().getPlayerOrException();
+        if (size > 0) {
+            BlockPos blockPos = serverPlayer.blockPosition();
+            Level level = serverPlayer.level;
+            BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
+            for (int x = -size; x < size; x++) {
+                for (int z = -size; z < size; z++) {
+                    BlockPos.MutableBlockPos pos = mutableBlockPos.setWithOffset(blockPos, x, -1, z);
+                    if (x % 5 == 0 && z % 5 == 0) {
+                        // 放置水和荷叶
+                        level.setBlockAndUpdate(pos, Blocks.WATER.defaultBlockState());
+                        level.setBlockAndUpdate(pos.move(Direction.UP), Blocks.LILY_PAD.defaultBlockState());
+                    } else {
+                        // 放置农田和作物
+                        level.setBlockAndUpdate(pos, Blocks.FARMLAND.defaultBlockState().setValue(FarmBlock.MOISTURE, 7));
+                        level.setBlockAndUpdate(pos.move(Direction.UP), Blocks.WHEAT.defaultBlockState().setValue(CropBlock.AGE, 7));
+                    }
+                }
+            }
+        }
+
         return Command.SINGLE_SUCCESS;
     }
 }
