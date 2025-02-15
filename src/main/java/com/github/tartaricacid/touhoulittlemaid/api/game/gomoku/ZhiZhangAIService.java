081808a6f15028dce8143908b2a857ea451b7022
tartaric_acid <baka943@qq.com>
2025-02-15 20:42:44 +0800

    部分格式修正

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/api/game/gomoku/ZhiZhangAIService.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/api/game/gomoku/ZhiZhangAIService.java
@@ -1099,10 +1099,11 @@ public class ZhiZhangAIService implements AIService {
                 if (point.score >= alpha) {
                     if (isRoot) {
                         if (point.score > alpha) {
-                            // 找到了更好的落子点，将之前选择的落子点清空
                             // 第一步估值不准确，所以不清空
-                            if(this.rounds <= 1)
+                            if (this.rounds <= 1) {
+                                // 找到了更好的落子点，将之前选择的落子点清空
                                 bestPointList.clear();
+                            }
                         }
                         // 记录该落子点
                         bestPointList.add(point);
@@ -1139,11 +1140,16 @@ public class ZhiZhangAIService implements AIService {
         }
 
         if (isRoot) {
-            // 如果有多个落子点，则通过getBestPoint方法选择一个最好的
             int count = bestPointList.size();
-            if(count == 1) this.bestPoint = bestPointList.get(0);
-            else if(this.rounds > 1) this.bestPoint = getRandomBestPoint(bestPointList);
-            else this.bestPoint = getBestPoint(bestPointList);
+            if (count == 1) {
+                this.bestPoint = bestPointList.get(0);
+            } else if (this.rounds > 1) {
+                // 如果回合数大于 1，则获取随机最佳或次佳点位
+                this.bestPoint = getRandomBestPoint(bestPointList);
+            } else {
+                // 否则，则通过 getBestPoint 方法选择一个最好的
+                this.bestPoint = getBestPoint(bestPointList);
+            }
         }
 
         int score = isAI ? alpha : beta;
@@ -1401,7 +1407,6 @@ public class ZhiZhangAIService implements AIService {
      * 从给定的点位列表中获取随机最佳或次佳点位
      *
      * @param pointList 点位列表
-     * @return
      */
     private Point getRandomBestPoint(List<Point> pointList) {
         Point bestPoint = null;
@@ -1420,7 +1425,9 @@ public class ZhiZhangAIService implements AIService {
                 secondPoint = point;
             }
         }
-        if (secondPoint == null)    return bestPoint;
+        if (secondPoint == null) {
+            return bestPoint;
+        }
 
         return Math.random() < 0.5 ? bestPoint : secondPoint;
     }
