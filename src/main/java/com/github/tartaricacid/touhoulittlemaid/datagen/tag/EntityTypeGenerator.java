8ef8e40d8229533d0579920650a65988e577c7ac
tartaric_acid <baka943@qq.com>
2025-03-21 13:16:17 +0800

    将女仆添加进石化黑名单中

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/tag/EntityTypeGenerator.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/tag/EntityTypeGenerator.java
@@ -5,7 +5,10 @@ import com.github.tartaricacid.touhoulittlemaid.init.InitEntities;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
 import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.tags.TagKey;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.common.data.ForgeEntityTypeTagsProvider;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class EntityTypeGenerator extends EntityTypeTagsProvider {
     public EntityTypeGenerator(DataGenerator generator, ExistingFileHelper existingFileHelper) {
@@ -17,5 +20,8 @@ public class EntityTypeGenerator extends EntityTypeTagsProvider {
         tag(EntityTypeTags.IMPACT_PROJECTILES).add(InitEntities.DANMAKU.get());
         tag(EntityTypeTags.POWDER_SNOW_WALKABLE_MOBS).add(InitEntities.FAIRY.get());
         tag(EntityTypeTags.FREEZE_IMMUNE_ENTITY_TYPES).add(InitEntities.FAIRY.get());
+//        tag(EntityTypeTags.FALL_DAMAGE_IMMUNE).add(InitEntities.FAIRY.get());
+        tag(TagKey.create(ForgeRegistries.ENTITY_TYPES.getRegistryKey(), new ResourceLocation("iceandfire", "immune_to_gorgon_stone")))
+                .add(InitEntities.MAID.get());
     }
 }
