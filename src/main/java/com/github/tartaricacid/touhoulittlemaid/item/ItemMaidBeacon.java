1eaf0d981105f4c551ac30726038235a4115cf18
QiChen <2102733232@qq.com>
2025-08-10 13:58:35 +0800

    修复神社亭灯由物品放置时无法正确读取数据的问题

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/item/ItemMaidBeacon.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/item/ItemMaidBeacon.java
@@ -46,8 +46,10 @@ public class ItemMaidBeacon extends DoubleHighBlockItem {
 
     public static void itemStackToTileEntity(ItemStack stack, TileEntityMaidBeacon beacon) {
         CompoundTag tag = stack.get(STORAGE_DATA_TAG);
-        if (tag != null) {
-            beacon.loadData(tag);
+        if (tag != null&& tag.contains(NEO_FORGE_DATA_TAG, Tag.TAG_COMPOUND)) {
+            //正确读取tag数据
+            CompoundTag forgeTag = tag.getCompound(NEO_FORGE_DATA_TAG);
+            beacon.loadData(forgeTag);
         }
     }
 
