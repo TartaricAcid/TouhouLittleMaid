68f328f5fda3b02b68b06812da331e56ccb3d73f
tartaric_acid <baka943@qq.com>
2025-09-29 00:57:42 +0800

    修正 mixin 错误

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/mixin/NodeEvaluatorBurningCacher.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/mixin/NodeEvaluatorBurningCacher.java
@@ -6,13 +6,13 @@ import net.minecraft.core.HolderSet;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.pathfinder.NodeEvaluator;
+import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(NodeEvaluator.class)
+@Mixin(WalkNodeEvaluator.class)
 @SuppressWarnings("all")
 public class NodeEvaluatorBurningCacher {
     @Inject(method = "isBurningBlock", at = @At("HEAD"), cancellable = true)
