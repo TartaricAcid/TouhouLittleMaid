e855702bc9d76a5432342e56e1832f9446765583
tartaric_acid <baka943@qq.com>
2026-01-20 16:45:16 +0800

    祭坛构建检查更加宽松，现在只要点击中间羊毛方块的任意朝向都可以构建

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/item/ItemHakureiGohei.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/item/ItemHakureiGohei.java
@@ -26,7 +26,6 @@ import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 
@@ -70,31 +69,47 @@ public class ItemHakureiGohei extends ProjectileWeaponItem {
 
     @Override
     public InteractionResult useOn(UseOnContext context) {
-        if (context.getHand() == InteractionHand.MAIN_HAND) {
+        if (context.getHand() == InteractionHand.MAIN_HAND && context.getLevel() instanceof ServerLevel serverLevel) {
             List<IMultiBlock> multiBlockList = MultiBlockManager.getMultiBlockList();
             BlockState blockState = context.getLevel().getBlockState(context.getClickedPos());
-            Level world = context.getLevel();
             BlockPos pos = context.getClickedPos();
-            Direction direction = context.getClickedFace();
 
-            for (IMultiBlock multiBlock : multiBlockList) {
-                if (multiBlock.isCoreBlock(blockState) && multiBlock.directionIsSuitable(direction)) {
-                    if (world instanceof ServerLevel serverLevel) {
-                        BlockPos posStart = pos.offset(multiBlock.getCenterPos(direction));
-                        StructureTemplate template = multiBlock.getTemplate(serverLevel, direction);
-                        if (multiBlock.isMatch(world, posStart, direction, template)) {
-                            multiBlock.build(world, posStart, direction, template);
-                            serverLevel.playSound(null, pos, SoundEvents.BEACON_ACTIVATE, SoundSource.BLOCKS, 1.5f, 1);
-                            if (context.getPlayer() instanceof ServerPlayer serverPlayer) {
-                                InitTrigger.MAID_EVENT.trigger(serverPlayer, TriggerType.BUILD_ALTAR);
-                            }
-                        }
+            // 检查水平四个方向
+            for (Direction direction : Direction.Plane.HORIZONTAL) {
+                // 有可能玩家点击的是右侧，故需要判断两个位置
+                BlockPos leftPos = pos.relative(direction.getClockWise());
+                BlockState leftBlockState = context.getLevel().getBlockState(leftPos);
+
+                for (IMultiBlock multiBlock : multiBlockList) {
+                    if (multiBlock.isCoreBlock(blockState)
+                        && multiBlock.directionIsSuitable(direction)
+                        && this.checkAndBuild(context, multiBlock, serverLevel, pos, direction)) {
+                        return InteractionResult.SUCCESS;
                     }
-                    return InteractionResult.SUCCESS;
+
+                    if (multiBlock.isCoreBlock(leftBlockState)
+                        && multiBlock.directionIsSuitable(direction)
+                        && this.checkAndBuild(context, multiBlock, serverLevel, leftPos, direction)) {
+                        return InteractionResult.SUCCESS;
+                    }
                 }
             }
         }
         return super.useOn(context);
+    }
+
+    private boolean checkAndBuild(UseOnContext context, IMultiBlock multiBlock, ServerLevel world, BlockPos pos, Direction direction) {
+        BlockPos posStart = pos.offset(multiBlock.getCenterPos(direction));
+        StructureTemplate template = multiBlock.getTemplate(world, direction);
+        if (multiBlock.isMatch(world, posStart, direction, template)) {
+            multiBlock.build(world, posStart, direction, template);
+            world.playSound(null, pos, SoundEvents.BEACON_ACTIVATE, SoundSource.BLOCKS, 1.5f, 1);
+            if (context.getPlayer() instanceof ServerPlayer serverPlayer) {
+                InitTrigger.MAID_EVENT.trigger(serverPlayer, TriggerType.BUILD_ALTAR);
+            }
+            return true;
+        }
+        return false;
     }
 
     @Override
