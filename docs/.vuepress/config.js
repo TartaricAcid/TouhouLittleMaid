bfc6bd9e8b94a354756d08a2c602e1ea743aeb56
tartaric_acid <baka943@qq.com>
2026-04-06 23:15:28 +0800

    修改 base url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ import {defineUserConfig} from "vuepress";
 import {plumeTheme} from "vuepress-theme-plume";
 
 export default defineUserConfig({
-    base: "/TouhouLittleMaid/",
+    base: "/",
     lang: "zh-CN",
     locales: {
         "/": {
